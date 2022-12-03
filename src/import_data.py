1a602e5e4480a1e7b47616a039d84a6c03228b35
lagillenwater <lagillenwater@gmail.com>
2022-12-03 09:14:00 -0700

    addded import function and wrapper

--- src/import_data.py
+++ src/import_data.py
@@ -1,3 +1,30 @@
-from torch_geometric_signed_directed.data import load_directed_real_data
+### program to import network tuple files and convert to Directed unsigned class
+import torch
+from torch_geometric.data import Data
+import argparse
 
-dataset = load_directed_real_data(dataset='telegram', root='./tmp_data/')
+
+#Define arguments for each required and optional input
+def define_arguments():
+    parser=argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    ## Required inputs
+    parser.add_argument("--input-dir",dest="InputDir",required=True,help="InputDir")
+    return parser
+
+# Wrapper function
+def generate_arguments(parser):
+
+    #Generate argument parser and define arguments
+    parser = define_arguments()
+    args = parser.parse_args()
+
+    input_dir = args.InputDir   
+    return input_dir
+
+def loadgraphs(input_dir):
+    
+
+
+
+
---
+++ src/wrapper.py
@@ -0,0 +1,12 @@
+#### wrapper script
+from import_data import *
+
+
+def main():
+
+    # read in files
+    input_dir = generate_arguments()
+    print(input_dir)
+
+if __name__ == '__main__':
+    main()
